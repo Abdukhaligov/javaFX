ad209014678302c1e923f7fdc33443d8385e6471
Abdukhaligov <hikmat.pou@Gmail.com>
2020-01-08 00:42:34 +0400

    SetDisable buttons & add keyPressHandler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,8 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
@@ -9,8 +11,34 @@ public class Controller {
     private TextField name;
 
     @FXML
-    public void onButtonClicked(){
-        System.out.println("Hello, " + name.getText());
+    private Button helloButton;
+
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+
+        if (e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + name.getText());
+        } else if (e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + name.getText());
+        }
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = name.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
     }
 
 }
