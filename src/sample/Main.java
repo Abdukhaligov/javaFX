9c519053f107a0d5d52d2d9a97a583108110e8f2
Abdukhaligov <hikmat.pou@Gmail.com>
2020-01-07 07:12:52 +0400

    Creating label via code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,16 +2,28 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(5);
+        root.setHgap(5);
+
+        Label greeting = new Label("Welcome to JavaFX");
+        root.getChildren().add(greeting);
+
+        primaryStage.setTitle("Hello JavaFx!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
