9a27d329b3af94a6013ad7331093a76b817d07bd
Abdukhaligov <hikmat.pou@Gmail.com>
2020-01-08 00:56:35 +0400

    UI Thread

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,18 +3,20 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 
 public class Controller {
 
     @FXML
     private TextField name;
-
     @FXML
     private Button helloButton;
-
     @FXML
     private Button byeButton;
+    @FXML
+    private CheckBox checkboxClearing;
+
 
     @FXML
     public void initialize() {
@@ -30,6 +32,19 @@ public class Controller {
         } else if (e.getSource().equals(byeButton)) {
             System.out.println("Bye, " + name.getText());
         }
+
+        try {
+            Thread.sleep(10000);
+        }catch (InterruptedException event){
+
+        }
+
+        if(checkboxClearing.isSelected()){
+            name.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
+
     }
 
     @FXML
@@ -39,6 +54,10 @@ public class Controller {
 
         helloButton.setDisable(disableButtons);
         byeButton.setDisable(disableButtons);
+    }
+
+    public void handleChange(){
+        System.out.println("The checkbox is " + (checkboxClearing.isSelected() ? "Checked" : "Not checked"));
     }
 
 }
